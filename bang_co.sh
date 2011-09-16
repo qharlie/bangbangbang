f9ffdc0fe47f2282a2cd66ae47e997fb31037eba
charles sanders <charlie.fats@gmail.com>
2011-09-16 14:35:40 -0500

    Updating check out script

--- bang_co.sh
+++ bang_co.sh
@@ -4,12 +4,13 @@ echo $node
 kill -9 $node
 # remove formhog
 rm -rf bangbang
-#checkout 
+#checkout
 git clone git@github.com:qbert65536/bangbang.git
+#DB setup
 rm -rf bang
 mv bangbang/website bang
 mv bangbang/install.sh .
 cd bangbang && tar czvf latest.tgz script/ && mv latest.tgz ..
 #start node and quit
-cd - && cd bang && nohup node app.js &
-
+cd - && cd bang && forevert start -o bang.log app.js &
+cd - && cd formhog && forever start -o fh.log app.js &
